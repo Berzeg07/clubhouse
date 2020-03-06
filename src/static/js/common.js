d291c5badf56b4568808345ed7e9d082337efbf8
Aslan <berezg86@gmail.com>
2020-03-06 15:24:10 +0300

    Табы на главной десктоп

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+
+
     $('.burger').click(function() {
         $(this).toggleClass('is-active');
         $('.dropdown-menu').toggleClass('is-active');
@@ -17,9 +20,37 @@ $(document).ready(function() {
         });
     });
 
-    var swiper = new Swiper('.main-slider', {
+    var tabSlider = new Swiper('.tab-slider', {
+        simulateTouch: false,
+        pagination: {
+            el: '.swiper-pagination_main',
+            clickable: true,
+            renderBullet: function(index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next_main',
+            prevEl: '.swiper-button-prev_main',
+        },
+    });
+
+    var mainSlider = new Swiper('.main-slider', {
         spaceBetween: 0,
         effect: 'fade',
+        loop: 'true',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var planSlider = new Swiper('.plan-slider', {
+        spaceBetween: 0,
         loop: 'true',
         pagination: {
             el: '.swiper-pagination',
@@ -30,5 +61,17 @@ $(document).ready(function() {
             prevEl: '.swiper-button-prev',
         },
     });
+
+    $('.plan-tabs__links a').click(function(e) {
+        e.preventDefault();
+        $('.plan-tabs__links a').removeClass('is-active');
+        $(this).addClass('is-active');
+        var tab = $(this).attr('href');
+        $('.tab-cont').not(tab).css({
+            'display': 'none'
+        });
+        $(tab).fadeIn(400);
+    });
+    $('.plan-tabs__links a:first').click();
 
 });
