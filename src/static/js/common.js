a0ecd734deca951b2b07b708814c188918c27c29
betokov <betokov93@mail.ru>
2020-03-10 02:48:26 +0300

    Правки

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -179,6 +179,47 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 
+    /*Print*/
+    // $('.apartaments__schemes .print').each(function(i, item) {
+    //     $(this).click(function() {
+    //         PrintElem('.apartaments__schemes');
+    //     });
+    // });
+
+    // function PrintElem(elem)
+    // {
+    //     Popup($(elem).html());
+    // }
+
+
+    // function Popup(data)
+
+    // {
+
+    //     var mywindow = window.open('', 'my div', 'height=600, width=960');
+
+    //     mywindow.document.write('<html><head><title>my div</title>');
+
+    //     mywindow.document.write('<link rel="stylesheet" href="css/main.css" type="text/css" />');
+
+    //     mywindow.document.write('</head><body >');
+
+    //     mywindow.document.write(data);
+
+    //     mywindow.document.write('</body></html>');
+
+    //     mywindow.document.close(); // necessary for IE >= 10
+
+    //     mywindow.focus(); // necessary for IE >= 10
+
+    //     mywindow.print();
+
+    //     // mywindow.close();
+
+    //     return true;
+    // }
+
+
     // Area maps *
     var show = $('.table-block');
     var showTop = show.offset().top;
