ee62cb1a71adf6b557d5e9a5c74183e5ce8f8a5c
Aslan <berezg86@gmail.com>
2020-03-09 16:29:55 +0300

    Блок с маркером на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,158 +1,172 @@
     $(document).ready(function() {
 
-        $(".plan-btn").click(function(e){
+        $('.flat-info__btn').click(function() {
+            var $this = $(this);
+            $('.flat-info__front').fadeOut();
+            $('.flat-info__back').fadeIn();
+            if (!$this.hasClass("active")) {
+                $('.flat-info__front').fadeIn();
+                $('.flat-info__back').fadeOut();
+            }
+
+            $($this).toggleClass('active');
+        });
+
+        $(".plan-btn").click(function(e) {
             e.preventDefault();
             var $this = $(this);
 
-            if( !$this.hasClass("active")){
-               $(".plan-mob__item").slideUp();
-               $(".plan-btn").removeClass("active");
-           }
-
-           $this.toggleClass("active");
-           $this.next().slideToggle();
-
-       });
-            // $(".plan-btn:first").click();
-
-            $('.burger').click(function() {
-                $(this).toggleClass('is-active');
-                $('.dropdown-menu').toggleClass('is-active');
-                $('body').toggleClass('overflow-hidden');
-            });
-
-            $(function() {
-                $(window).scroll(function() {
-                    if ($(this).scrollTop() >= 96) {
-                        $('.header__bottom').addClass('stickytop');
-                        $('.header').addClass('header-height-fixed');
-                    } else {
-                        $('.header__bottom').removeClass('stickytop');
-                        $('.header').removeClass('header-height-fixed');
-                    }
-                });
-            });
-
-            var tabSlider = new Swiper('.tab-slider', {
-                simulateTouch: false,
-                pagination: {
-                    el: '.swiper-pagination_main',
-                    clickable: true,
-                    renderBullet: function(index, className) {
-                        return '<span class="' + className + '">' + (index + 1) + '</span>';
-                    },
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next_main',
-                    prevEl: '.swiper-button-prev_main',
-                },
-            });
-
-            var mainSlider = new Swiper('.main-slider', {
-                spaceBetween: 0,
-                effect: 'fade',
-                loop: 'true',
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            });
-
-            var planSlider = new Swiper('.plan-slider', {
-                spaceBetween: 0,
-                loop: 'true',
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            });
-
-            // $('.plan-tabs__links a').click(function(e) {
-            //     e.preventDefault();
-            //     $('.plan-tabs__links a').removeClass('is-active');
-            //     $(this).addClass('is-active');
-            //     var tab = $(this).attr('href');
-            //     $('.tab-cont').not(tab).css({
-            //         'display': 'none'
-            //     });
-            //     $(tab).fadeIn(400);
-            // });
-            // $('.plan-tabs__links a:first').click();
-
-            /*partners carousel*/
-            if ( $(window).width() < 768 ) {
-                $('.partners-block').removeClass('owl-carousel').removeClass('owl-carousel2');
+            if (!$this.hasClass("active")) {
+                $(".plan-mob__item").slideUp();
+                $(".plan-btn").removeClass("active");
             }
 
-            $('.owl-carousel2').owlCarousel({
-                loop:true,
-                nav:true,
-                responsive:{
-                    768:{
-                        items: 2
-                    },
-                    992: {
-                        items: 3
-                    },
-                    1920: {
-                        items: 7
-                    }
-                }
-            });
-
-            $('.owl-carousel3').owlCarousel({
-                loop:true,
-                nav:true,
-                responsive:{
-                    0:{
-                        items: 1
-                    },
-
-                    768: {
-                        items: 2
-                    },
-                    1200: {
-                        items: 3
-                    }
-                }
-            });
-
-            $('.js-modal').click(function() {
-                $('.apartament-modal').fadeIn();
-                if($(window).width() < 1280) {
-                    $('body').css({'overflow' : 'hidden'});
-                    $('.overlay').css('display', 'block');
-                }
-            });
-
-            $('.js-modal-close').click(function() {
-                $('.apartament-modal').fadeOut();
-
-                 if($(window).width() < 1280) {
-                    $('body').css({'overflow' : 'scroll'});
-                    $('.overlay').css('display', 'none');
-                }
-           });
-
-             $('.js-tabs a').click(function(e) {
-                e.preventDefault();
-                
-
-                $('.js-tabs a').each(function(i, item) {
-                        $(this).removeClass('active');
-                });
-
-                $(this).addClass('active');
-            });
+            $this.toggleClass("active");
+            $this.next().slideToggle();
 
         });
+        // $(".plan-btn:first").click();
+
+        $('.burger').click(function() {
+            $(this).toggleClass('is-active');
+            $('.dropdown-menu').toggleClass('is-active');
+            $('body').toggleClass('overflow-hidden');
+        });
+
+        $(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() >= 96) {
+                    $('.header__bottom').addClass('stickytop');
+                    $('.header').addClass('header-height-fixed');
+                } else {
+                    $('.header__bottom').removeClass('stickytop');
+                    $('.header').removeClass('header-height-fixed');
+                }
+            });
+        });
+
+        var tabSlider = new Swiper('.tab-slider', {
+            simulateTouch: false,
+            pagination: {
+                el: '.swiper-pagination_main',
+                clickable: true,
+                renderBullet: function(index, className) {
+                    return '<span class="' + className + '">' + (index + 1) + '</span>';
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-next_main',
+                prevEl: '.swiper-button-prev_main',
+            },
+        });
+
+        var mainSlider = new Swiper('.main-slider', {
+            spaceBetween: 0,
+            effect: 'fade',
+            loop: 'true',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+
+        var planSlider = new Swiper('.plan-slider', {
+            spaceBetween: 0,
+            loop: 'true',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+        //
+        $('#tabsLink a').click(function(e) {
+            e.preventDefault();
+            $('#tabsLink a').removeClass('is-active');
+            $(this).addClass('is-active');
+            var tab = $(this).attr('href');
+            $('.tab-cont').not(tab).css({
+                'display': 'none'
+            });
+            $(tab).fadeIn(400);
+        });
+        $('#tabsLink a:first').click();
+
+        /*partners carousel*/
+        if ($(window).width() < 768) {
+            $('.partners-block').removeClass('owl-carousel').removeClass('owl-carousel2');
+        }
+
+        $('.owl-carousel2').owlCarousel({
+            loop: true,
+            nav: true,
+            responsive: {
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1920: {
+                    items: 7
+                }
+            }
+        });
+
+        $('.owl-carousel3').owlCarousel({
+            loop: true,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+
+                768: {
+                    items: 2
+                },
+                1200: {
+                    items: 3
+                }
+            }
+        });
+
+        $('.js-modal').click(function() {
+            $('.apartament-modal').fadeIn();
+            if ($(window).width() < 1280) {
+                $('body').css({
+                    'overflow': 'hidden'
+                });
+                $('.overlay').css('display', 'block');
+            }
+        });
+
+        $('.js-modal-close').click(function() {
+            $('.apartament-modal').fadeOut();
+
+            if ($(window).width() < 1280) {
+                $('body').css({
+                    'overflow': 'scroll'
+                });
+                $('.overlay').css('display', 'none');
+            }
+        });
+
+        $('.js-tabs a').click(function(e) {
+            e.preventDefault();
 
 
+            $('.js-tabs a').each(function(i, item) {
+                $(this).removeClass('active');
+            });
+
+            $(this).addClass('active');
+        });
+
+    });
